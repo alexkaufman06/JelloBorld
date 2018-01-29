c523291549e2c6e6a1362d04598a2dc07a76b464
Alex Kaufman <alexkaufman06@gmail.com>
2018-01-28 19:17:59 -0800

    Add project files.

---
+++ JelloBorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// NAMESPACE IS A WAY OF ORGANIZING CODE
+namespace JelloBorld
+{
+    // BELOW IS A CLASS CALLED "Program"
+    // CLASSES ARE CONTAINERS FOR ALL OF THE METHODS IN YOUR PROGRAM
+    class Program
+    {
+    // BELOW IS A METHOD
+        static void Main(string[] args)
+        // THIS IS A CODE BLOCK CALLED "Main"
+        {
+            Console.WriteLine("Jello Borld!");
+            // READ LINE IS A METHOD THAT WAITS FOR THE USER TO PUSH ENTER
+            Console.ReadLine();
+            Console.WriteLine("What are you doing?");
+            Console.ReadLine();
+        }
+    }
+}
