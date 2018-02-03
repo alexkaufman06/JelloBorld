61bebbfacc86d58e437652d4bc5c1e5115ca7e72
Alex Kaufman <alexkaufman06@gmail.com>
2018-02-03 08:37:29 -0800

    Add type function

--- JelloBorld/Program.cs
+++ JelloBorld/Program.cs
@@ -4,25 +4,19 @@
    READ UP ON OOP */
 namespace JelloBorld
 {
-    // BELOW IS A CLASS CALLED "Program"
-    // CLASSES ARE CONTAINERS FOR ALL OF THE METHODS IN YOUR PROGRAM
-    class Program
+    class Program // CLASSES ARE CONTAINERS FOR ALL THE METHODS IN A PROGRAM
     {
     // BELOW IS A METHOD CALLED MAIN static=not changing | void=not returning a value
         static void Main(string[] args)
-        // THIS IS A CODE BLOCK CALLED "Main"
-        {
-            // CONSOLE IS A CLASS
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("Hello and welcome to the GREATEST GAME EVER CREATED! What's your name");
-            Console.Write("?");
-            // READ LINE IS A METHOD THAT WAITS FOR THE USER TO PUSH ENTER
+        { // THIS IS A CODE BLOCK CALLED "Main"
+            Console.ForegroundColor = ConsoleColor.Green; // CONSOLE IS A CLASS
+            Type("Hello and welcome to the GREATEST GAME EVER CREATED! What's your name?");
             Console.ResetColor();
-            string name = Console.ReadLine();
+            string name = Console.ReadLine(); // READ LINE IS A METHOD THAT WAITS FOR THE USER TO PUSH ENTER
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(name + "!? That's a stupid fucking name! Suit yourself loser...");
+            Type(name + "!? That's a stupid fucking name! Suit yourself loser...");
             System.Threading.Thread.Sleep(2500);
-            Console.WriteLine("Which direction would you like to go? (North, South, East, West)");
+            Type("Which direction would you like to go? (North, South, East, or West)");
             Choice1(name);
             //Donkey();
         }
@@ -35,6 +29,16 @@ namespace JelloBorld
             Console.ReadLine();
         }
         */
+        static void Type(string stringToType)
+        {
+            Random rnd = new Random();
+            for (int i = 0; stringToType.Length - 1 >= i; i++)
+            {
+                System.Threading.Thread.Sleep(rnd.Next(1,125));
+                Console.Write(stringToType[i]);
+            }
+            Console.WriteLine("");
+        }
         static void Choice1(string nameVal)
         {
             Console.ResetColor();
@@ -43,29 +47,29 @@ namespace JelloBorld
             Console.ForegroundColor = ConsoleColor.Green;
             if (direction == "North" || direction == "north")
             {
-                Console.WriteLine("You ran into a wall and suffered a traumatic brain injury...");
+                Type("You ran into a wall and suffered a traumatic brain injury...");
                 Death(name);
             }
             else if (direction == "South" || direction == "south")
             {
-                Console.WriteLine("You find yourself in a frozen tundra and get a gnarly case of frostbite...");
+                Type("You find yourself in a frozen tundra and get a gnarly case of frostbite...");
                 Death(name);
             }
             else if (direction == "East" || direction == "east")
             {
-                Console.WriteLine("The sun is rising and you can feel it's warmth. YOU WON THE GAME!");
+                Type("The sun is rising and you can feel it's warmth. YOU WON THE GAME!");
                 Console.ReadLine();
             }
             else if (direction == "West" || direction == "west")
             {
-                Console.WriteLine("The warmth of the sunrise can be felt on your back. An animal sneaks up and eats you...");
+                Type("The warmth of the sunrise can be felt on your back. An animal sneaks up and eats you...");
                 Death(name);
             }
             else
             {
-                Console.WriteLine("Please input a cardinal direction...");
+                Type("Please input a cardinal direction...");
                 System.Threading.Thread.Sleep(2500);
-                Console.WriteLine("YA DUMBASS!");
+                Type("YA DUMBASS!!!");
                 Choice1(name);
             }
         }
@@ -73,9 +77,9 @@ namespace JelloBorld
         {
             string name = nameVal;
             System.Threading.Thread.Sleep(3000);
-            Console.WriteLine("You die.");
+            Type("You die.");
             System.Threading.Thread.Sleep(1500);
-            Console.WriteLine("Try making better choices next time, " + name + "!!!");
+            Type("Try making better choices next time, " + name + "!!!");
             Console.ReadLine();
         }
     }
