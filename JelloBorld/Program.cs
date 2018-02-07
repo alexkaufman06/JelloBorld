a0ab1fc514fc7e7b73fb6020cb8f833c2d512aaf
Alex Kaufman <alexkaufman06@gmail.com>
2018-02-06 16:49:24 -0800

    Add switch statement

--- JelloBorld/Program.cs
+++ JelloBorld/Program.cs
@@ -2,14 +2,17 @@
 /* Solution Explorer on right (A Solution can contain multiple projects)
    NAMESPACE IS A WAY OF ORGANIZING CODE
    READ UP ON OOP */
+// Make class called item with parameters to hold onto things
+// Has height, width, weight (parent class)
+// Then make classes built off of the parent class that define a weapon, a wearable (backpack/armor), or gear (like a key)
+// Machine language vs assembly (read up)
 namespace JelloBorld
 {
     class Program // CLASSES ARE CONTAINERS FOR ALL THE METHODS IN A PROGRAM
-    {
-    // BELOW IS A METHOD CALLED MAIN static=not changing | void=not returning a value
+    {            // BELOW IS A METHOD CALLED MAIN static=not changing | void=not returning a value
         static void Main(string[] args)
         { // THIS IS A CODE BLOCK CALLED "Main"
-            Beginning:
+            Beginning: // THIS IS A LABEL USED TO RESTART THE GAME
             Console.ForegroundColor = ConsoleColor.Green; // CONSOLE IS A CLASS
             Matrix();
             Type("Hello and welcome to the GREATEST GAME EVER CREATED! What's your name?");
@@ -58,8 +61,7 @@ namespace JelloBorld
                 }
                 Console.Write(binary);
                 System.Threading.Thread.Sleep(60);
-            }
-            // BELOW SCROLLS THE BINARY OUT OF VIEW
+            } // BELOW SCROLLS THE BINARY OUT OF VIEW
             for (int i = 1; i <= 35; i++)
             {
                 System.Threading.Thread.Sleep(60);
@@ -73,41 +75,34 @@ namespace JelloBorld
             string direction = Console.ReadLine().ToLower(); 
             Console.WriteLine("");
             Console.ForegroundColor = ConsoleColor.Green;
-            if (direction == "north")
-                 // Turn if statement into case (maybe)
-                 // Make class called item with parameters to hold onto things
-                 // Has height, width, weight (parent class)
-                 // Then make classes built off of the parent class that define a weapon, a wearable (backpack/armor), or gear (like a key)
-                 // Machine language vs assembly (read up)
+            switch (direction)
             {
-                Type("You ran into a wall and suffered a traumatic brain injury...");
-                Death(nameVal);
-            }
-            else if (direction == "south")
-            {
-                Type("You find yourself in a frozen tundra and get a gnarly case of frostbite...");
-                Death(nameVal);
-            }
-            else if (direction == "east")
-            {
-                Type("The sun is rising and you can feel its warmth. YOU WON THE GAME! Not only did you win the " +
+                case "north":
+                    Type("You ran into a wall and suffered a traumatic brain injury...");
+                    Death(nameVal);
+                    break;
+                case "south":
+                    Type("You find yourself in a frozen tundra and get a gnarly case of frostbite...");
+                    Death(nameVal);
+                    break;
+                case "east":
+                    Type("The sun is rising and you can feel its warmth. YOU WON THE GAME! Not only did you win the " +
                      "game, but you also succeeded in re-routing the encryptor while hacking into the mainframe!");
-                System.Threading.Thread.Sleep(2500);
-                Matrix();
-                Type("Great job " + nameVal + "!!!");
-                System.Threading.Thread.Sleep(2500);
-            }
-            else if (direction == "west")
-            {
-                Type("The warmth of the sunrise can be felt on your back. An animal sneaks up and eats you...");
-                Death(nameVal);
-            }
-            else
-            {
-                Type("Please input a cardinal direction...");
-                System.Threading.Thread.Sleep(2500);
-                Type("YA DUMBASS!!!");
-                Choice1(nameVal);
+                    System.Threading.Thread.Sleep(2500);
+                    Matrix();
+                    Type("Great job " + nameVal + "!!!");
+                    System.Threading.Thread.Sleep(2500);
+                    break;
+                case "west":
+                    Type("The warmth of the sunrise can be felt on your back. An animal sneaks up and eats you...");
+                    Death(nameVal);
+                    break;
+                default:
+                    Type("Please input a cardinal direction...");
+                    System.Threading.Thread.Sleep(2500);
+                    Type("YA DUMBASS!!!");
+                    Choice1(nameVal);
+                    break;
             }
         }
         static void Death(string nameVal)
